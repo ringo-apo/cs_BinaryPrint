98dc2fc52b1ba4a2e109aed74af662fdbdc9d2a5
unknown <taira_akira@A-TAIRA-PC.nago_etech.local>
2019-02-27 10:21:47 +0900

    201902271028

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using System.IO;
 
 namespace ConsoleApplication1
@@ -12,21 +7,51 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int b=0;
-            byte[] data = File.ReadAllBytes("D:filename.dat"); //表示したいファイル名
+            int b = 0;
+            string str1 = ""; //一行に表示する数値文字列
+            string str2 = ""; //一行に表示するキャラクタ文字列
+            string fileName = "";
+
+            Console.WriteLine(@"ファイル名をパスから入力してください。例）C:\hoge.txt");
+            fileName = Console.ReadLine();
+            byte[] data = File.ReadAllBytes(fileName); //表示したいファイル名
             foreach (int n in data) {
-                if (b == 7)
+
+                str1 += n.ToString("X2");
+
+                if (n >= 33 && n <= 126)
                 {
-                    Console.Write('\n');
+                    str2 += (char)n;
+
+                }
+                else
+                {
+                    str2 += ".";
+                }
+
+                if (b == 15)
+                {
+
+                    Console.WriteLine(str1 + " : " + str2);
+                    str1 = "";
+                    str2 = "";
+
                     b = 0;
                 }
                 else
                 {
+                    str1 += " ";
+
                     b++;
                 }
-                Console.Write(n.ToString("X2"));
-                Console.Write(" " );
+
             }
+
+            Console.WriteLine(str1 + " : " + str2);
+
+            Console.WriteLine("");
+            Console.WriteLine("ファイルサイズ:" + data.Length.ToString() + "バイト");
+            Console.WriteLine("完了しました。リターンを押すと終了します。");
             Console.ReadLine();
         }
     }
