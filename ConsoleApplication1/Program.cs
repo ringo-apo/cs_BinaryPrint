ba471609fc8506b825002d4d432c80d2cbd5626e
unknown <taira_akira@A-TAIRA-PC.nago_etech.local>
2019-02-27 09:23:31 +0900

    201902270930

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b=0;
+            byte[] data = File.ReadAllBytes("D:filename.dat"); //表示したいファイル名
+            foreach (int n in data) {
+                if (b == 7)
+                {
+                    Console.Write('\n');
+                    b = 0;
+                }
+                else
+                {
+                    b++;
+                }
+                Console.Write(n.ToString("X2"));
+                Console.Write(" " );
+            }
+            Console.ReadLine();
+        }
+    }
+}
